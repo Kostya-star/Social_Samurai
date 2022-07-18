15a83287d69a8c801a7cc1a54e02c12cae61dce0
Constantin <konstantindanilov19@hotmail.com>
2022-07-18 15:21:56 +0300

    learning react 71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,21 +1,19 @@
 import Preloader from "../../common/preloader/Preloader";
 import s from "./ProfileInfo.module.css";
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) return <Preloader/>
   return (
     <div>
-      <div>
-        <img
-          className={s.nature_img}
-          src="https://picfiles.alphacoders.com/278/278586.jpg"
-          alt="im an img"
-        />
-      </div>
+      {/* <div>
+        <img className={s.nature_img} src="https://picfiles.alphacoders.com/278/278586.jpg"/>
+      </div> */}
       <div className={s.descriptionBlock}> 
         <img src={props.profile.photos.large} /> <br/>
         About me: {props.profile.aboutMe} <br/>
         Looking for a job: {props.profile.lookingForAJobDescription}
+        <ProfileStatus status={'yo whussup'}/>  
       </div>
     </div>
   );
